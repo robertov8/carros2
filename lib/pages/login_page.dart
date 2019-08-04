ae6c29ebe8ebf005388eddbf639ce11a104ed7f0
Roberto Ribeiro <robertov82008@gmail.com>
2019-08-04 18:09:38 -0300

    80 - Tratando os eventos do botão de Login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  final _tLogin = TextEditingController(text: 'roberto');
+  final _tSenha = TextEditingController(text: '123');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,35 +21,68 @@ class LoginPage extends StatelessWidget {
     return Form(
       child: ListView(
         children: <Widget>[
-          TextFormField(
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              labelText: 'Login',
-              labelStyle: TextStyle(color: Colors.blue, fontSize: 25),
-              hintText: 'Digite o seu login',
-              hintStyle: TextStyle(color: Colors.black, fontSize: 18),
-            ),
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 25,
-            ),
-          ),
-          TextFormField(
-            obscureText: true,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Senha',
-              labelStyle: TextStyle(color: Colors.blue, fontSize: 25),
-              hintText: 'Digite a sua senha',
-              hintStyle: TextStyle(color: Colors.black, fontSize: 18),
-            ),
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 25,
-            ),
-          )
+          _textFormFieldLogin(),
+          _textFormFieldPassword(),
+          _loginButton(),
         ],
       ),
     );
+  }
+
+  TextFormField _textFormFieldLogin() {
+    return TextFormField(
+      controller: _tLogin,
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        labelText: 'Login',
+        labelStyle: TextStyle(color: Colors.black, fontSize: 25),
+        hintText: 'Digite o seu login',
+        hintStyle: TextStyle(color: Colors.black, fontSize: 18),
+      ),
+      style: TextStyle(
+        color: Colors.blue,
+        fontSize: 25,
+      ),
+    );
+  }
+
+  TextFormField _textFormFieldPassword() {
+    return TextFormField(
+      controller: _tSenha,
+      obscureText: true,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: 'Senha',
+        labelStyle: TextStyle(color: Colors.black, fontSize: 25),
+        hintText: 'Digite a sua senha',
+        hintStyle: TextStyle(color: Colors.black, fontSize: 18),
+      ),
+      style: TextStyle(
+        color: Colors.blue,
+        fontSize: 25,
+      ),
+    );
+  }
+
+  _loginButton() {
+    return Container(
+      height: 50,
+      margin: EdgeInsets.only(top: 20),
+      child: RaisedButton(
+        onPressed: () => _onClickLogin(),
+        color: Colors.blue,
+        child: Text(
+          'Login',
+          style: TextStyle(color: Colors.white, fontSize: 25),
+        ),
+      ),
+    );
+  }
+
+  _onClickLogin() {
+    final login = _tLogin.text;
+    final senha = _tSenha.text;
+
+    print('Login: $login, $senha');
   }
 }
