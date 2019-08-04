a3514e1a3ca08176fc204fb41c8989a9aae3c5df
Roberto Ribeiro <robertov82008@gmail.com>
2019-08-04 17:33:49 -0300

    79 - Formulário de login (Form/TextFormField)

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,8 +7,45 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Carros'),
       ),
-      body: Container(
-        color: Colors.orange,
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: _body(),
+      ),
+    );
+  }
+
+  _body() {
+    return Form(
+      child: ListView(
+        children: <Widget>[
+          TextFormField(
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              labelText: 'Login',
+              labelStyle: TextStyle(color: Colors.blue, fontSize: 25),
+              hintText: 'Digite o seu login',
+              hintStyle: TextStyle(color: Colors.black, fontSize: 18),
+            ),
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 25,
+            ),
+          ),
+          TextFormField(
+            obscureText: true,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Senha',
+              labelStyle: TextStyle(color: Colors.blue, fontSize: 25),
+              hintText: 'Digite a sua senha',
+              hintStyle: TextStyle(color: Colors.black, fontSize: 18),
+            ),
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 25,
+            ),
+          )
+        ],
       ),
     );
   }
