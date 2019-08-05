08faf63c77549b2eca5814ece3652516944e9fe8
Roberto Ribeiro <robertov82008@gmail.com>
2019-08-05 14:28:17 -0300

    88 - Lista de carros - layout (Card)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:carros/domain/carro.dart';
 import 'package:carros/domain/services/carro_service.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +9,10 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Carros'),
       ),
-      body: _listView(),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: _listView(),
+      ),
     );
   }
 
@@ -20,13 +24,54 @@ class HomePage extends StatelessWidget {
         itemBuilder: (BuildContext context, index) {
           final carro = carros[index];
 
-          return Text(
+          return Container(
+            height: 150,
+            child: Card(
+              child: _listTile(carro),
+            ),
+          );
+        });
+  }
+
+  _listTile(Carro carro) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ListTile(
+          leading: Image.network(
+            carro.urlFoto,
+            width: 150,
+          ),
+          title: Text(
             carro.nome,
             style: TextStyle(
               color: Colors.blue,
               fontSize: 25,
             ),
-          );
-        });
+          ),
+          subtitle: Text(
+            "descrição",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        ButtonTheme.bar(
+          child: ButtonBar(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {},
+                child: const Text('DETALHES'),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: const Text('SHARE'),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
